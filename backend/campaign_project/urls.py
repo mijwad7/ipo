b19b6a94cc521125de32f7d45d31a77b7db54c93
Muhammed Mijwad <mijuzz007@gmail.com>
2025-12-12 01:59:46 +0530

    urls

--- backend/campaign_project/urls.py
+++ backend/campaign_project/urls.py
@@ -7,5 +7,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('onboarding.urls')),
-    re_path(r"^(?!api/).*", TemplateView.as_view(template_name="index.html")),
+    re_path(r"^(?!api/|admin/|static/|media/|assets/).*", TemplateView.as_view(template_name="index.html")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
